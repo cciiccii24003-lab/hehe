84012a209296df9542f695b6a7d104bfb7366f09
cciiccii24003-lab <cciiccii24003@gmail.com>
2026-07-23 16:18:19 +0700

    Add files via upload

---
+++ PCareService/script.js
@@ -0,0 +1,19 @@
+function capek(){
+
+document.getElementById("hasil").innerHTML = `
+
+<img src="images/hati.jpeg" class="panda">
+
+<h2>Lovvv youuu moreeeeee 🤍</h2>
+
+<p>Kamuu semangatt yaaaa kerjanyaaa kan calon ceo emang gituuu hihiii akuuu seneng jadi pacarr kamuu bahkan saat kamu sibuk kamu msi bisaa ngasih kabar huuu
+   kamuuu jangann bosen bosenn yaakk samaa akuuu hoooo </p>
+
+
+<button class="peluk">
+🫂 Peyukkkk
+</button>
+
+`;
+
+}
